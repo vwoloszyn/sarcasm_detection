5bccc4a9908ce333709b768a02371b2e2a8386da
Vinicius Woloszyn <vinicius@open.inf.br>
2016-11-03 14:00:15 -0200

    crowler to get quotes from goodreads.com

---
+++ web_crawler.py
@@ -0,0 +1,17 @@
+
+
+max_pages=27
+
+
+def get_quotes(tot_pag, tag, ):
+	quotes=[]
+	for i in xragne(max_pages):
+		url="https://www.goodreads.com/quotes/tag/"+str(tag)+"?page="+str(i)+"&utf8=%E2%9C%93"
+		
+		#here you can use beatiful
+		#http://www.pythonforbeginners.com/python-on-the-web/web-scraping-with-beautifulsoup
+
+	return quotes
+
+
+
