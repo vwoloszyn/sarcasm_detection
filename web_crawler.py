3fa24193e17b99d24670c9b92df51a43119ec34d
Mario Jose Kunz Filho <mariofilho4k@gmail.com>
2016-11-14 01:19:47 -0200

    Update web_crawler.py

--- web_crawler.py
+++ web_crawler.py
@@ -3,11 +3,13 @@
 max_pages=27
 
 
-def get_quotes(tot_pag, tag, ):
+def get_quotes(max_pages, tag, ):
 	quotes=[]
 	for i in xragne(max_pages):
 		url="https://www.goodreads.com/quotes/tag/"+str(tag)+"?page="+str(i)+"&utf8=%E2%9C%93"
-		
+		from bs4 import BeautifulSoup
+		soup=BeautifulSoup(url,'html.parser')
+                soup=soup.find()
 		#here you can use beatiful
 		#http://www.pythonforbeginners.com/python-on-the-web/web-scraping-with-beautifulsoup
 
